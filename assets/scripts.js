13f8afec8ebe60ce797b3ebec9bfe2c3fdc5b9d6
PJ Bergeron <pierrejeanbergeron@gmail.com>
2020-04-30 15:34:43 +0200

    Update style

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,14 +3,18 @@ var userLng = null;
 var p = new Ping()
 
 var servers = [
-  { name: 'Europe', region: 'eu-west-1', lat: 53, lng: -8, popup: null, data: null },
-  { name: 'NA East', region: 'us-east-1', lat: 38.13, lng: -78.45, popup: null, data: null },
-  { name: 'NA West', region: 'us-west-1', lat: 37.35, lng: -121.96, popup: null, data: null },
-  { name: 'South America', region: 'sa-east-1', lat: -23.34, lng: -46.38, popup: null, data: null },
-  { name: 'Middle East', region: 'me-south-1', lat: 26.07, lng: 50.56, popup: null, data: null },
-  { name: 'Japan', region: 'ap-northeast-1', lat: 35.41, lng: 139.42, popup: null, data: null },
-  { name: 'Oceania', region: 'ap-southeast-2', lat: -33.86, lng: 151.2, popup: null, data: null }
+  { name: 'Europe', region: 'eu-west-1', lat: 53, lng: -8, marker: null, latency: null, previousLatency: null },
+  { name: 'NA East', region: 'us-east-1', lat: 38.13, lng: -78.45, marker: null, latency: null, previousLatency: null },
+  { name: 'NA West', region: 'us-west-1', lat: 37.35, lng: -121.96, marker: null, latency: null, previousLatency: null },
+  { name: 'Brazil', region: 'sa-east-1', lat: -23.34, lng: -46.38, marker: null, latency: null, previousLatency: null },
+  { name: 'Middle East', region: 'me-south-1', lat: 26.07, lng: 50.56, marker: null, latency: null, previousLatency: null },
+  { name: 'Asia', region: 'ap-northeast-1', lat: 35.41, lng: 139.42, marker: null, latency: null, previousLatency: null },
+  { name: 'Oceania', region: 'ap-southeast-2', lat: -33.86, lng: 151.2, marker: null, latency: null, previousLatency: null }
 ]
+
+function getRandomId() {
+  return Math.random().toString(36).substring(7)
+}
 
 function getUrl(region) {
   return 'https://ec2.' + region + '.amazonaws.com/ping'
@@ -22,68 +26,65 @@ function runPing(server, map, index, callback) {
   })
 }
 
-function addLine(to, index, map) {
-  // Add line
-  var lineGeoJson = {
-    'type': 'FeatureCollection',
-    'features': [
-      {
-        'type': 'Feature',
-        'geometry': {
-          'coordinates': [[userLng, userLat], to],
-          'type': 'LineString'
+function addUser(lat, lng, map) {
+  map.loadImage(
+    '/assets/user.png',
+    function(error, image) {
+      if (error) throw error;
+      map.addImage('user', image);
+      map.addSource('point', {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': [
+            {
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Point',
+                'coordinates': [lng, lat]
+              }
+            }
+          ]
         }
-      }
-    ]
-  }
-
-    var id = Math.random().toString(36).substring(7)
-
-    map.addSource('line' + id, {
-      type: 'geojson',
-      lineMetrics: true,
-      data: lineGeoJson
-    })
-
-    map.addLayer({
-      type: 'line',
-      source: 'line' + id,
-      id: 'line' + id,
-      paint: {
-        'line-width': 6,
-        'line-gradient': [
-          'interpolate',
-          ['linear'],
-          ['line-progress'],
-          0,
-          'blue',
-          1,
-          'purple'
-        ]
-      },
-      layout: {
-        'line-cap': 'round',
-        'line-join': 'round'
-      }
-    })
+      })
+      map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': 'point',
+        'layout': {
+          'icon-image': 'user',
+          'icon-size': 1
+        }
+      })
+    }
+  )
 }
 
 function addServer(data, map, index) {
-  // Only replace popup if lower ping or first ping
-  if (servers[index].data == null || servers[index].data > data) {
-    // Remove previous version of popup
-    if (servers[index].popup) {
-      servers[index].popup.remove()
-    } else {
-      addLine([servers[index].lng, servers[index].lat], index, map)
+  // Only replace marker if lower ping or first ping
+  if (servers[index].latency == null || servers[index].latency > data) {
+    // Remove previous version of marker
+    if (servers[index].marker) {
+      servers[index].marker.remove()
     }
 
-    servers[index].data = data
+    servers[index].latency = data
 
-    servers[index].popup = new mapboxgl.Popup({ closeOnClick: false, closeButton: false })
-    .setLngLat([servers[index].lng, servers[index].lat])
-    .setHTML(servers[index].name + '<br><strong>' + parseInt(data) + 'ms</strong>')
-    .addTo(map)
+    var el = document.createElement('div')
+    el.className = 'marker'
+    el.style.display = 'block'
+    el.innerHTML = servers[index].name + '<br><strong>' + parseInt(data) + 'ms</strong>'
+
+    if (parseInt(data) <= 40)
+      el.classList.add('white')
+    else if (parseInt(data) <= 150)
+      el.classList.add('orange')
+    else
+      el.classList.add('red')
+
+    servers[index].marker = new mapboxgl.Marker(el)
+      .setLngLat([servers[index].lng, servers[index].lat])
+      .addTo(map)
   }
 }
 
@@ -97,15 +98,11 @@ $(function() {
     zoom: 1.5
   })
 
-  // Add servers to map and rerun every second
-  var ran = 0
+  // Add servers to map and rerun every 2 seconds
   var interval = setInterval(function() {
     servers.forEach(function(server, index) {
       runPing(getUrl(server), map, index, addServer)
     })
-
-    if (++ran === 10)
-      window.clearInterval(interval)
   }, 2000)
 
   // Add user to map and center map
@@ -113,12 +110,7 @@ $(function() {
     userLat = data.latitude
     userLng = data.longitude
 
-    /*
-    // Create marker
-    var marker = new mapboxgl.Marker()
-    .setLngLat([userLng, userLat])
-    .addTo(map)
-    */
+    addUser(userLat, userLng, map)
 
     // Center map on marker
     map.flyTo({ center: [userLng, userLat] })
